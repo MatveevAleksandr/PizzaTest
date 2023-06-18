ad52157d46123a29035b1bc4b0d258cbfc2df24b
Aleksander <matveev.alexander96@gmail.com>
2023-06-18 14:10:51 +0300

    added banner block hide when scrolling

--- app/src/main/java/com/hammersystems/myapplication/pages/main_menu/adapters/MenuCategoryListRecyclerAdapter.kt
+++ app/src/main/java/com/hammersystems/myapplication/pages/main_menu/adapters/MenuCategoryListRecyclerAdapter.kt
@@ -4,16 +4,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.hammersystems.myapplication.R
 
 class MenuCategoryListRecyclerAdapter(
     private val categoryList: List<String>, private val categoryClick: (String) -> Unit
 ) : RecyclerView.Adapter<MenuCategoryListRecyclerAdapter.MenuCategoryViewHolder>() {
+
+    private val menuCategoryCardList = mutableListOf<MenuCategoryCardView>()
+
     class MenuCategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val categoryName: TextView = itemView.findViewById(R.id.category_name)
-        val categoryCard: CardView = itemView.findViewById(R.id.category_card)
+        val categoryCard: MaterialCardView = itemView.findViewById(R.id.category_card)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuCategoryViewHolder {
@@ -23,8 +27,12 @@ class MenuCategoryListRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: MenuCategoryViewHolder, position: Int) {
+        val menuCategoryCardItem =
+            MenuCategoryCardView(card = holder.categoryCard, txt = holder.categoryName)
+        menuCategoryCardList.add(menuCategoryCardItem)
         holder.categoryName.text = categoryList[position]
         holder.categoryCard.setOnClickListener {
+            focusOnItem(menuCategoryCardItem)
             categoryClick(categoryList[position])
         }
     }
@@ -32,4 +40,17 @@ class MenuCategoryListRecyclerAdapter(
     override fun getItemCount(): Int {
         return categoryList.size
     }
+
+    private fun focusOnItem(cardItem: MenuCategoryCardView) {
+        cardItem.card.setCardBackgroundColor("#33FD3A69".toColorInt())
+        cardItem.txt.setTextColor("#FD3A69".toColorInt())
+        menuCategoryCardList.forEach {
+            if (it != cardItem) {
+                it.card.setCardBackgroundColor("#FFFFFF".toColorInt())
+                it.txt.setTextColor("#C3C4C9".toColorInt())
+            }
+        }
+    }
 }
+
+data class MenuCategoryCardView(val card: MaterialCardView, val txt: TextView)
